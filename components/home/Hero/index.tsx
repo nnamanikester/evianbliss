d9754e0a0aaf11d455a659e73a4a37e162dafd3c
nnamanikester <nnamanikester@gmail.com>
2022-09-14 08:12:18 +0100

    feat: responsive hero section

---
+++ components/home/Hero/index.tsx
@@ -0,0 +1,43 @@
+import Image from "next/future/image";
+import React from "react";
+import config from "../../../config";
+
+interface HeroProps {}
+
+export const Hero: React.FC<HeroProps> = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="col-5 content">
+          <h1 className="hero__title">{config.APP_NAME.toUpperCase()}</h1>
+          <h2 className="sub-title">in Enugu.</h2>
+
+          <ul className="my-4">
+            <li>
+              <span className="icon lni lni-certificate" />
+              Hair specialists of the highest category
+            </li>
+            <li>
+              <span className="icon lni lni-diamond-alt" />
+              Premium Hair brands
+            </li>
+            <li>
+              <span className="icon lni lni-crown" />
+              Common quality and service standards
+            </li>
+            <li>
+              <span className="icon lni lni-protection" />
+              Customer saftey
+            </li>
+          </ul>
+        </div>
+        <div className="col-8 hero-image-container">
+          <Image
+            src={require("../../../public/images/evian-bliss-hair-model-2.jpg")}
+            alt="Evian Bliss Hair Model Hero Image"
+          />
+        </div>
+      </section>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,13 @@
 import type { NextPage } from "next";
 import Image from "next/future/image";
 import NextImage from "next/image";
+import { Hero } from "../components/home";
 import config from "../config";
 
 const Home: NextPage = () => {
   return (
     <div className="home">
-      <section className="hero">
-        <div className="col-5 content">
-          <div>
-            <h1 className="hero__title">{config.APP_NAME.toUpperCase()}</h1>
-            <h2 className="sub-title">in Enugu.</h2>
-
-            <ul className="my-4">
-              <li>
-                <span className="icon lni lni-certificate" />
-                Hair specialists of the highest category
-              </li>
-              <li>
-                <span className="icon lni lni-diamond-alt" />
-                Premium Hair brands
-              </li>
-              <li>
-                <span className="icon lni lni-crown" />
-                Common quality and service standards
-              </li>
-              <li>
-                <span className="icon lni lni-protection" />
-                Customer saftey
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-8 hero-image-container"></div>
-      </section>
+      <Hero />
 
       <section className="appointment container">
         <div className="py-3 px-4">
