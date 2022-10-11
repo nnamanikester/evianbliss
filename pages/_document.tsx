4f2d36c9750419203b12dedf26345c9ebc14daa7
nnamanikester <nnamanikester@gmail.com>
2022-10-11 22:59:08 +0100

    feat: added google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   return (
     <Html lang="en-US">
       <Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-XYS74MFZ79"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-XYS74MFZ79');
+          `}
+        </Script>
         <link
           href="https://cdn.lineicons.com/3.0/lineicons.css"
           rel="stylesheet"
