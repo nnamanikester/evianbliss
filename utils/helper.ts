203d0660dd9e1e848e45ac6c26e68dd8f799ec75
nnamanikester <nnamanikester@gmail.com>
2022-10-14 17:25:17 +0100

    feat: modified generateTimeSlots function

--- utils/helper.ts
+++ utils/helper.ts
@@ -26,18 +26,8 @@ export const generateTimeSlots = (date: string) => {
     // add the average duration to get the next available time
     i = i.add(DURATION, "hours")
   ) {
-    // Checks if the date is the current day.
-    if (dayjs().isSame(dayjs(date), "day")) {
-      // Generate the time only when its still available,
-      // if not, start from the next available hour
-      if (dayjs().isBefore(i, "hour")) {
-        // Add the next available hour to the time slots
-        slots.push({
-          time: i.format("hh:mm A"),
-          date: i.format("YYYY-MM-DD"),
-        });
-      }
-    } else {
+    // if not, start from the next available hour
+    if (dayjs().isBefore(i, "hour")) {
       // Add the next available hour to the time slots
       slots.push({
         time: i.format("hh:mm A"),
