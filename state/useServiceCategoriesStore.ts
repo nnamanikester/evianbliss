81893bad3fd45f78c240da2d4e7c8120f27b8b01
nnamanikester <nnamanikester@gmail.com>
2022-10-18 12:41:20 +0100

    feat: sorting categories

--- state/useServiceCategoriesStore.ts
+++ state/useServiceCategoriesStore.ts
@@ -14,7 +14,7 @@ export const useServiceCategoriesStore = create<ServiceCategoriesStore>(
     categories: [],
     setCategories: (categories) => {
       const sorted = categories.sort((a, b) =>
-        a.created_at < b.created_at ? 1 : -1
+        a.created_at < b.created_at ? -1 : 1
       );
       set({ categories: sorted });
     },
